266c62a12405d9febfc21bc9e8d5bdc6aa0f2e07
Marcos Cozatti <marcoscozatti@gmail.com>
2020-09-16 20:26:20 -0300

    primeira versao

---
+++ metodo_e_funcao/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using metodo_e_funcao;
+using System.Data.SqlClient;
+
+
+namespace metodo_e_funcao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Abrir banco de dados");
+            Controles cnn = new Controles();
+            Console.WriteLine(cnn.Con());
+            try
+            {
+                Console.WriteLine("banco de dados aberto");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("agora temos a segunda versao com esta frase");
+                Console.WriteLine("terceira atualizacao do sistema");
+                Console.WriteLine();
+            }
+        }
+    }
+}
